05ed8f6df7bd1bd2f6b213648791861aab473796
fujitatsu4952 <fujitatsu49528@gmail.com>
2019-11-05 19:41:36 +0900

    hello

---
+++ js/main.js
@@ -0,0 +1,50 @@
+(function() {
+  'use strict';
+
+  // const nothing = document.querySelector('textarea');
+  // nothing.addEventListener('mousehover' , e => {
+  //   nothing.textContent =''
+  // })
+
+  var vm = new Vue({
+    el: '#app',
+    data: {
+      country: '',
+      selected: '',
+      dated:'',
+      address:'都道府県から記述してください',
+      work:'',
+      tell:'',
+      people:'',
+      name:''
+    },
+    
+    methods: {
+      // addItem: function(e) {
+      //   e.preventDefault();
+      //   this.todos.push(this.country);
+      // }
+      completeItem: function() {
+        console.log(this.country);
+        console.log(this.selected);
+        console.log(this.dated);
+        console.log(this.address);
+        console.log(this.work);
+        console.log(this.tell);
+        console.log(this.people);
+        console.log(this.name);
+      }
+    }
+  });
+})();
+
+
+
+
+// const a = document.querySelectorAll('div');
+//   const button = document.querySelector('button');
+
+//   button.addEventListener('click', e => {
+//     e.preventDefault();
+//     console.log(a);
+//   });
